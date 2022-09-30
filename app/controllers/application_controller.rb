488ad1546dec1c2d45ac308f6a52e3be590a1f59
koechronix <koechronix@yahoo.com>
2022-09-30 14:44:50 +0300

    session and cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::API
+  # this is added to access cookies 
+  # since all controllers inherit from this controller
   include ActionController::Cookies
 end
